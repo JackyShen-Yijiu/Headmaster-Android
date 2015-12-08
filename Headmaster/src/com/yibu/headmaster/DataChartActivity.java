c1703b215d5101dc67a28ba2724a3f9b18523b35
nfp5424 <316391801@qq.com>
2015-12-08 15:16:39 +0800

    清单文件添加anctivity

--- Headmaster/src/com/yibu/headmaster/DataChartActivity.java
+++ Headmaster/src/com/yibu/headmaster/DataChartActivity.java
@@ -1,17 +1,31 @@
 package com.yibu.headmaster;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.support.v4.view.ViewPager;
 import android.view.View;
+
+import com.yibu.headmaster.adapter.MoreDataAdapter;
+import com.yibu.headmaster.base.impl.MoreDataPager;
+import com.yibu.headmaster.lib.PagerSliding.PagerSlidingTab;
 
 public class DataChartActivity extends BaseActivity {
 
 	private View view;
+	private List<MoreDataPager> tabPagers;
+	private PagerSlidingTab slidingTab;
+	private ViewPager viewPager;
 
 	@Override
 	protected void initView() {
-		// TODO Auto-generated method stub
-		view = View.inflate(getBaseContext(), R.layout.activity_data_chart,
-				null);
+		view = View
+				.inflate(getBaseContext(), R.layout.activity_more_data, null);
 		content.addView(view);
+		slidingTab = (PagerSlidingTab) view
+				.findViewById(R.id.more_data_sliding_tab);
+		viewPager = (ViewPager) view.findViewById(R.id.more_data_view_pager);
+
 	}
 
 	@Override
@@ -22,6 +36,15 @@ public class DataChartActivity extends BaseActivity {
 	@Override
 	protected void initData() {
 
+		// 初始化数据
+		tabPagers = new ArrayList<MoreDataPager>();
+		String[] title = new String[] { "今天", "昨天", "本周", "本月", "本年" };
+		for (int i = 0; i < title.length; i++) {
+			tabPagers.add(new MoreDataPager(getApplicationContext()));
+		}
+
+		viewPager.setAdapter(new MoreDataAdapter(title, tabPagers));
+		slidingTab.setViewPager(viewPager);
 	}
 
 	@Override
