6e2b95d841d9131efcd60ad918940c0aba1ca8c6
nfp5424 <316391801@qq.com>
2015-12-11 11:17:41 +0800

    聊天导入

---
+++ Headmaster/src/com/yibu/headmaster/PersonSettingActivity.java
@@ -0,0 +1,31 @@
+package com.yibu.headmaster;
+
+public class PersonSettingActivity extends BaseActivity {
+
+	@Override
+	protected void initView() {
+
+	}
+
+	@Override
+	protected void initListener() {
+
+		baseTitle.setText("个人信息");
+	}
+
+	@Override
+	protected void initData() {
+
+	}
+
+	@Override
+	public void processSuccess(String data) {
+
+	}
+
+	@Override
+	public void processFailure() {
+
+	}
+
+}
